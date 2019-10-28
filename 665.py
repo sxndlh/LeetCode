b132070d5578641ea7576369643470044908d5b2
sxndlh <804421401@qq.com>
2019-10-28 15:12:36 +0800

    new

--- 665.py
+++ 665.py
@@ -1,6 +1,10 @@
+# 先对边界特殊的头尾进行特殊判断
+# 之后对一般的中间部分进行限制
+# 判断条件为：将两个比较的数取成相同，两个数有一个满足其前后非递减就行
+# 注意控制判断条件间的逻辑关系
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        key = 1
+        key = 1   # 标志符，限制只修改一次
         if len(nums) < 3:
             return True
         for i in range(len(nums)-1):
